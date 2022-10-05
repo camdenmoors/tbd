3d976ca9e747ff1876fa937afeee81c47f091729
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-10-05 08:40:14 -0400

    Sync changes

--- apps/backend/src/executions/executions.service.ts
+++
@@ -1,9 +0,0 @@
-import {Injectable} from '@nestjs/common';
-import { Execution } from './types/execution';
-
-@Injectable()
-export class ExecutionsService {
-    async getExecutions(): Promise<Execution[]> {
-        return []
-    }
-}
--- apps/backend/src/profiles/types/profile.d.ts
+++
@@ -1,8 +0,0 @@
-export type Profile = {
-    name: string;
-    version: string;
-    description?: string;
-    author?: string;
-    license?: string;
-    tarball: string;
-}
